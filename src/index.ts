2748c4223fcb794cabdc79344763496fac1ff738
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2023-06-07 15:21:45 -0300

    Adding database support for Fastify

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import fastify from 'fastify'
+import MySQL from '@fastify/mysql'
 
 const server = fastify()
+
+server.register(MySQL, {
+  connectionString: 'mysql://hacker:aW50cnVkZXI=@localhost/owasp',
+})
 
 server.listen({ port: 3000 }, (err, address) => {
   err
